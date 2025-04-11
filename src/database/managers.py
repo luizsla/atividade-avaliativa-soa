f9fce81d4dcfdb626bd954d43740a4bdfe308b16
Luiz Eduardo Amorim <eduardogbi10@gmail.com>
2025-04-11 12:12:32 -0300

    Adicionando a dependência python decouple para servir variáveis de ambinete a partir de arquivos .env

--- src/database/managers.py
+++ src/database/managers.py
@@ -2,11 +2,16 @@ import psycopg
 
 from uuid import uuid4
 
+from decouple import config
+
 from .queries import CREATE_BOOK_QUERY, LIST_BOOKS_QUERY
 
 
+CON_STRING = config("DATABASE_CONNECTION_STRING")
+
+
 def list_books():
-    with psycopg.connect() as conn:
+    with psycopg.connect(CON_STRING) as conn:
         with conn.cursor() as cursor:
             cursor.execute(LIST_BOOKS_QUERY)
             books_rows = cursor.fetchall()
@@ -14,8 +19,9 @@ def list_books():
     return [{} for row in books_rows]
 
 
+
 def create_new_book(title, author, keywords, isbn_10):
-    with psycopg.connect() as conn:
+    with psycopg.connect(CON_STRING) as conn:
         with conn.cursor() as cursor:
             cursor.execute(CREATE_BOOK_QUERY, (str(uuid4()), title, author, keywords, isbn_10))
 
